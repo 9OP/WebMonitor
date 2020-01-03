2834b5e122f3399f04bb58ff4acaa554cea7a31a
Martin Grd <siamang.martin@gmail.com>
2020-01-04 00:04:34 +0100

    improve installation

--- install.sh
+++ install.sh
@@ -1,22 +1,33 @@
 #!/bin/sh
 
 # WebMonitor/  root path
-BASEDIR=$(dirname "$0")
+BASEDIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
 # INSTALL GI LIB and other necessary stuff if not already installed
 sudo apt install libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-3.0 python3-pip
 
 # INSTALL venv
-pip3 install venv
+pip3 install virtualenv
 
-# CREATE VIRTUAL ENV
+# CREATE VIRTUAL ENV (python3.3+)
 python3 -m venv ${BASEDIR}/venv/
 
 # INSTALL requirements for WebMonitor
 venv/bin/pip3 install -r ${BASEDIR}/resources/conf/requirements.txt
 
 # ADD app to $PATH
-PATH=$PATH:/home/siamang/Dev/WebMonitor/
+PATH=$PATH:${BASEDIR}
+
+echo "
+[Desktop Entry]
+Version=1.0
+Name=WebMonitor
+Icon=${BASEDIR}/resources/media/cctv.png
+Exec=${BASEDIR}/webmonitor
+Terminal=false
+Type=Application
+Categories=Utility;Application;
+" > webmonitor.desktop
 
 # ADD webmonitor.desktop to /usr/share/applications/
 sudo cp ${BASEDIR}/webmonitor.desktop /usr/share/applications
